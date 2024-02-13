343eb471761aaa60c97248d4020adc999ef9fd47
DOVENE AGOGUE <adovene@gmail.com>
2024-02-13 12:22:30 +0100

    update

--- src/com/dov/javapoo/Main.java
+++ src/com/dov/javapoo/Main.java
@@ -7,5 +7,10 @@ public class Main {
         david.setAge(15);
         System.out.println(david.getName() +" a "+david.getAge() + " ans");
 
+        Student max = new Student();
+        max.setName("Max");
+        max.setAge(25);
+        max.setCourseName("POO");
+        System.out.println(max.getName() +" a "+max.getAge() + " ans"+ "et est inscrit en "+max.getCourseName());
       }
 }
---
+++ src/com/dov/javapoo/Student.java
@@ -0,0 +1,13 @@
+package com.dov.javapoo;
+
+public class Student extends Person {
+    private String courseName;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
